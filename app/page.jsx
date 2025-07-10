471543f8e595c6fe71bf9a9437a9af5337877c12
wilzzzzzzzz <itswgxx@gmail.com>
2025-07-10 13:33:18 +0700

    chores: finish signup page

--- app/page.jsx
+++ app/page.jsx
@@ -29,20 +29,19 @@ export default function Home() {
           />
         </div>
         <div className="container flex flex-col gap-[40px] w-[514px] h-[132px] -ml-32 pt-8 pr-12">
-          <p className="text-[#543310] font-bold text-3xl text-center w">
-            Selamat datang di Sipintar, website penyedia pendidikan terbaik
-            se-Indonesia!
+          <p className="text-[#543310] font-bold text-3xl text-center">
+            Selamat datang di Sipintar! Aplikasi pembelajaran No.1 di Indonesia!
           </p>
           <div className="flex flex-col justify-center items-center gap-[40px]">
             <Button
               onClick={handleSignUp}
-              className="bg-[#543310] transition duration-300 h-14 text-xl text-white font-bold rounded-full w-[340px] cursor-pointer border-b-6 border-b-[#AF8F6F] p-2 active:border-b-3 hover:bg-[#54331099] hover:scale-105"
+              className="bg-[#543310] duration-300 h-14 text-xl text-white font-bold rounded-full w-[340px] cursor-pointer border-b-6 border-b-[#AF8F6F] p-2 active:border-b-3 hover:bg-[#54331099] hover:scale-105"
             >
               Mulai Belajar
             </Button>
             <Button
               onClick={handleSignIn}
-              className="bg-white transiiton h-14 duration-300 text-xl text-[#543310] font-bold rounded-full w-[340px] cursor-pointer p-2 border-3 border-b-8 border-[#543310] active:border-b-3 hover:scale-105 hover:bg-neutral-100"
+              className="bg-white h-14 duration-300 text-xl text-[#543310] font-bold rounded-full w-[340px] cursor-pointer p-2 border-3 border-b-8 border-[#543310] active:border-b-3 hover:scale-105 hover:bg-neutral-100"
             >
               Sudah punya akun
             </Button>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/navbar";
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function SignUp() {
   return (
     <div>
       <Navbar />
-      <div className="flex">
+      <div className="flex gap-[40px]">
         <Image src={"/mountain.png"} width={766} height={848} alt="mountains" />
         <div className="flex flex-col  w-[35%]">
           <h1 className="font-bold text-center text-4xl">
@@ -18,38 +19,45 @@ export default function SignUp() {
             <div className="flex flex-col">
               <label htmlFor="">Username</label>
               <input
-                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
+                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 opacity-50 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
                 type="text"
-                placeholder="Your username"
+                placeholder="Username Anda"
               />
             </div>
 
             <div className="flex flex-col">
               <label htmlFor="">Email</label>
               <input
-                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
+                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 opacity-50 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
                 type="email"
-                placeholder="example@gmail.com"
+                placeholder="contoh@gmail.com"
               />
             </div>
 
             <div className="flex flex-col">
               <label htmlFor="">Password</label>
               <input
-                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
+                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 opacity-50 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
                 type="password"
-                placeholder="Your password"
+                placeholder="Password Anda"
               />
             </div>
 
             <div className="flex flex-col">
               <label htmlFor="">Konfirmasi Password</label>
               <input
-                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
+                className="h-14 p-4 rounded-full bg-[#AF8F6F] border-3 opacity-50 border-[#543310] focus:border-red-500 placeholder:text-[#54331090]"
                 type="password"
-                placeholder="Your password"
+                placeholder="Konfirmasi Password Anda"
               />
             </div>
+            <Button
+              type="submit"
+              className="bg-[#543310] duration-300 h-14 text-xl text-white font-bold rounded-full w-full cursor-pointer border-b-6 border-b-[#AF8F6F] p-2 active:border-b-3 hover:bg-[#54331099] hover:scale-105"
+            >
+              Daftar
+            </Button>
+            
           </form>
         </div>
       </div>
