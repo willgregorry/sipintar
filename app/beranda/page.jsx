ff7e4ac63226b9561092783393d7c94f007e3a0b
wilzzzzzzzz <itswgxx@gmail.com>
2025-08-10 01:00:56 +0700

    chore: update dashboard page

--- app/beranda/page.jsx
+++ app/beranda/page.jsx
@@ -2,15 +2,18 @@ import Appbar from "@/components/appbar";
 import materiData from "@/data/materi.json";
 import LevelItem from "@/components/levelItem";
 import Image from "next/image";
+import Link from "next/link";
 import Roadmap from "@/components/roadmap";
 
 export default function Beranda() {
   const materi = materiData[0];
   const limit = materiData[1];
+  const turunan = materiData[2];
+  const integral = materiData[3];
 
   return (
     <div>
-      <div className="px-44 mt-24 h-1000">
+      <div className="px-44 mt-24 h-auto mb-24">
         <Appbar />
 
         {/* Roadmap */}
@@ -39,24 +42,30 @@ export default function Beranda() {
         {/* Level List */}
         <div className="space-y-10 mt-24 z-0">
           {materi.level.map((lvl, i) => {
+            const isLocked = lvl.status === 'locked';
             const isEven = i % 2 === 0;
-            const isLastItem = i === materi.level.length - 1;
+
+            const wrapperClassName = `
+              relative 
+              ${isEven ? "pr-[70%]" : "pl-[0%]"} 
+              ${isLocked ? "opacity-50" : "transition-transform duration-300"}
+            `;
 
             return (
-              <div
-                key={lvl.nama}
-                className={`
-                    relative z-0
-                    ${isEven ? "pr-[70%]" : "pl-[0%]"}
-                  `}
-              >
+              <div key={lvl.slug} className={wrapperClassName}>
+              {isLocked ? (
                 <LevelItem nama={lvl.nama} status={lvl.status} />
-              </div>
+              ) : (
+                <Link href={`/beranda`/*`/materi/${lvl.slug}`*/} legacyBehavior>
+                  <a className="cursor-pointer">
+                    <LevelItem nama={lvl.nama} status={lvl.status} />
+                  </a>
+                </Link>
+              )}
+            </div>
             );
           })}
         </div>
-
-
 
         {/* Limit */}
         {/* Header Materi */}
@@ -79,19 +88,119 @@ export default function Beranda() {
         {/* Level List */}
         <div className="space-y-10 mt-24 z-0">
           {limit.level.map((lvl, i) => {
+            const isLocked = lvl.status === 'locked';
             const isEven = i % 2 === 0;
-            const isLastItem = i === limit.level.length - 1;
+
+            const wrapperClassName = `
+              relative 
+              ${isEven ? "pr-[70%]" : "pl-[0%]"} 
+              ${isLocked ? "opacity-50" : "transition-transform duration-300"}
+            `;
 
             return (
-              <div
-                key={lvl.nama}
-                className={`
-                    relative z-0
-                    ${isEven ? "pr-[70%]" : "pl-[0%]"}
-                  `}
-              >
+              <div key={lvl.slug} className={wrapperClassName}>
+              {isLocked ? (
                 <LevelItem nama={lvl.nama} status={lvl.status} />
-              </div>
+              ) : (
+                <Link href={`/beranda`/*`/materi/${lvl.slug}`*/} legacyBehavior>
+                  <a className="cursor-pointer">
+                    <LevelItem nama={lvl.nama} status={lvl.status} />
+                  </a>
+                </Link>
+              )}
+            </div>
+            );
+          })}
+        </div>
+
+        {/* Turunan */}
+        {/* Header Materi */}
+        <div className="bg-accent-theme mt-20 text-white p-4 rounded-lg flex justify-between items-center w-[65%]">
+          <div className="w-[40%] space-y-2 px-4 py-2">
+            <h1 className="text-[28px] font-bold">{turunan.materi}</h1>
+            <p className="text-md font-light">{turunan.deskripsi}</p>
+          </div>
+          <button className="flex leading-none items-center bg-[#AF8F6F] duration-300 h-auto text-xl text-white font-bold rounded-md w-40 cursor-pointer border-3 border-b-8 border-[#CAB5A0] border-b-[#CAB5A0] p-2 active:border-b-4 hover:bg-[#AF8F6F] hover:scale-105">
+            <Image
+              src="/assets/paper.png"
+              alt="materi"
+              width={50}
+              height={60}
+            />
+            <p className="text-accent-theme">Akses Materi</p>
+          </button>
+        </div>
+
+        {/* Level List */}
+        <div className="space-y-10 mt-24 z-0">
+          {turunan.level.map((lvl, i) => {
+            const isLocked = lvl.status === 'locked';
+            const isEven = i % 2 === 0;
+
+            const wrapperClassName = `
+              relative 
+              ${isEven ? "pr-[70%]" : "pl-[0%]"} 
+              ${isLocked ? "opacity-50" : "transition-transform duration-300"}
+            `;
+
+            return (
+              <div key={lvl.slug} className={wrapperClassName}>
+              {isLocked ? (
+                <LevelItem nama={lvl.nama} status={lvl.status} />
+              ) : (
+                <Link href={`/beranda`/*`/materi/${lvl.slug}`*/} legacyBehavior>
+                  <a className="cursor-pointer">
+                    <LevelItem nama={lvl.nama} status={lvl.status} />
+                  </a>
+                </Link>
+              )}
+            </div>
+            );
+          })}
+        </div>
+
+        {/* Integral */}
+        {/* Header Materi */}
+        <div className="bg-accent-theme mt-20 text-white p-4 rounded-lg flex justify-between items-center w-[65%]">
+          <div className="w-[40%] space-y-2 px-4 py-2">
+            <h1 className="text-[28px] font-bold">{integral.materi}</h1>
+            <p className="text-md font-light">{integral.deskripsi}</p>
+          </div>
+          <button className="flex leading-none items-center bg-[#AF8F6F] duration-300 h-auto text-xl text-white font-bold rounded-md w-40 cursor-pointer border-3 border-b-8 border-[#CAB5A0] border-b-[#CAB5A0] p-2 active:border-b-4 hover:bg-[#AF8F6F] hover:scale-105">
+            <Image
+              src="/assets/paper.png"
+              alt="materi"
+              width={50}
+              height={60}
+            />
+            <p className="text-accent-theme">Akses Materi</p>
+          </button>
+        </div>
+
+        {/* Level List */}
+        <div className="space-y-10 mt-24 z-0">
+          {integral.level.map((lvl, i) => {
+            const isLocked = lvl.status === 'locked';
+            const isEven = i % 2 === 0;
+
+            const wrapperClassName = `
+              relative 
+              ${isEven ? "pr-[70%]" : "pl-[0%]"} 
+              ${isLocked ? "opacity-50" : "transition-transform duration-300"}
+            `;
+
+            return (
+              <div key={lvl.slug} className={wrapperClassName}>
+              {isLocked ? (
+                <LevelItem nama={lvl.nama} status={lvl.status} />
+              ) : (
+                <Link href={`/beranda`/*`/materi/${lvl.slug}`*/} legacyBehavior>
+                  <a className="cursor-pointer">
+                    <LevelItem nama={lvl.nama} status={lvl.status} />
+                  </a>
+                </Link>
+              )}
+            </div>
             );
           })}
         </div>
--- components/levelItem.jsx
+++ components/levelItem.jsx
@@ -9,7 +9,22 @@ export default function LevelItem({ nama, status }) {
 
   return (
     <div className="flex flex-col items-center">
-      <Image src={iconSrc} alt={nama} width={180} height={160} />
+      <div className="relative">
+
+        <Image src={iconSrc} alt={nama} width={180} height={160} />
+
+        {status === "current" && (
+          <div className="absolute top-[-40px] left-1/2 -translate-x-1/2 -z-50">
+            <Image
+              src="/assets/sipintar.png"
+              alt="Current Level Bear"
+              width={220}
+              height={22+0}
+            />
+          </div>
+        )}
+      </div>
+
       <p className="mt-2 text-sm text-center">{nama}</p>
     </div>
   );
